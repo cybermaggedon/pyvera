605a960a4d310e29f49c5a94e8d9b8bb3665d600
Mark Adams <cybermaggedon@gmail.com>
2020-12-31 12:46:23 +0000

    Added Python packaging

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyvera",
+    version="1.0",
+    author="Cybermaggedon",
+    author_email="mark@cyberapocalypse.co.uk",
+    description="Python library to allow control of a Vera home automation hub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cybermaggedon/pyvera",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    scripts=[
+        "scripts/vera-get-weather",
+        "scripts/vera-update-scenes",
+        "scripts/vera-get-scenes",
+        "scripts/vera-delete-scenes"
+    ]
+)
+
